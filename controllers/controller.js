22f5ce26fb677ce32bebe2c10783fc51e11f0ba6
archiesuyono <archiesuyono@gmail.com>
2026-02-26 16:56:42 +0700

    progress pp

--- controllers/controller.js
+++ controllers/controller.js
@@ -6,7 +6,10 @@ class Controller {
     //halaman login
     static async getLogin(req,res) {
         try {
-            
+            if (req.session.UserId) {
+                return res.redirect("/explore")
+            }
+            res.render("login")
         } catch (error) {
             res.send(error)
         }
@@ -14,7 +17,21 @@ class Controller {
     //fungsi login
     static async postLogin(req,res) {
         try {
-            
+            const { email, password } = req.body
+            const user = await User.findOne({ where: { email }})
+
+            if (!user) {
+                return res.render("login", { error: "Email is not registered" })
+            }
+
+            const isMatch = Helper.comparePassword(password, user.password)
+
+            if (!isMatch) {
+                return res.render("login", { error: "Invalid password"})
+            }
+
+            req.session.UserId = user.id
+            res.redirect("/explore")
         } catch (error) {
             res.send(error)
         }
@@ -22,7 +39,11 @@ class Controller {
     //halaman register
     static async getRegister(req,res) {
         try {
-            
+            if (req.session.userId) {
+                return res.redirect("/explore")
+            }
+
+            res.render("register")
         } catch (error) {
             res.send(error)
         }
@@ -30,17 +51,27 @@ class Controller {
     //submit register
     static async postRegister(req,res) {
         try {
-            
+            const { username, email, password } = req.body
+            const newUser = await User.create({username, email, password})
+
+            await Profile.create({ UserId: newUser.id })
+            res.redirect("/login")
         } catch (error) {
-            res.send(error)
+            res.render("register", { error: error.errors?.[0]?.message || error.message })
         }
     }
 
     //halaman home (ada form buat status)
     static async getExplore(req,res) {
         try {
-
-            res.render("explore")
+            const post = await Post.findAll({
+                include: [
+                    { model: User },
+                    { model: Tag },
+                ],
+                order: [["createdAt", "DESC"]]
+            })
+            res.render("explore", { posts })
         } catch (error) {
             res.send(error)
         }
@@ -49,7 +80,22 @@ class Controller {
     //Posting status
     static async postExplore(req,res) {
         try {
+            const { content } = req.body
+            const newPost = await Post.create({
+                content,
+                UserId: req.session.UserId
+            })
             
+            const hashtags = content.match(/#\w+/g)
+            if (hashtags) {
+                for (const tag of hashtags) {
+                    const [tagInstance] = await Tag.findOrCreate({
+                        where: { name: tag }
+                    })
+                    await newPost.addTag(tagInstance)
+                }
+            }
+            res.redirect("/explore")
         } catch (error) {
             res.send(error)
         }
@@ -58,7 +104,16 @@ class Controller {
     //halaman search
     static async getSearch(req,res) {
         try {
-            
+            const { q } = req.query
+            const posts = await Post.findAll({
+                where: {
+                    content: {
+                        [Op.like]: `%${q}%`
+                    }
+                },
+                include: [{ model: User }]
+            })
+            res.render("search", { posts, keyword: q })
         } catch (error) {
             res.send(error)
         }
@@ -66,7 +121,23 @@ class Controller {
     //halaman profile
     static async getProfile(req,res) {
         try {
-            
+            const { id } = req.params
+            const user = await User.findByPk(id, {
+                include: [
+                    { model: Profile },
+                    {
+                        model: Post,
+                        include: [{ model: Tag }]
+                    }
+                ]
+            })
+
+            if (!user) {
+                return res.render("404", { message: "User is not found" })
+            }
+
+            const isOwner = req.session.UserId == id
+            res.render("profile", { user, isOwner })
         } catch (error) {
             res.send(error)
         }
@@ -75,7 +146,10 @@ class Controller {
     //halaman edit profile
     static async getEditProf(req,res) {
         try {
-            
+            const profile = await Profile.findOne({
+                where: { UserId: req.session.userId }
+            })
+            res.render("editProfile", { profile })
         } catch (error) {
             res.send(error)
         }
@@ -84,7 +158,13 @@ class Controller {
     //submit edit profile
     static async postEditProf(req,res) {
         try {
-            
+            const { bio, profilePicture } = req.body
+            await Profile.update(
+                { bio, profilePicture },
+                { where: { UserId: req.session.userId }}
+            )
+
+            res.redirect(`/profile/${req.session.userId}`)
         } catch (error) {
             res.send(error)
         }
@@ -93,7 +173,20 @@ class Controller {
     //tampilan post/status
     static async getPost(req,res) {
         try {
-            
+            const { id } = req.params
+            const post = await Post.findByPk(id, {
+                include: [
+                    { model: User },
+                    { model: Tag }
+                ]
+            })
+
+            if (!post) {
+                return res.render("404", { message: "Post is not found" })
+            }
+
+            const isOwner = req.session.userId == post.UserId
+            res.render("post", { post, isOwner })
         } catch (error) {
             res.send(error)
         }
@@ -101,7 +194,32 @@ class Controller {
     //submit post/status yang diedit
     static async postEditPost(req,res) {
         try {
-            
+            const { id } = req.params
+            const { content } = req.body
+            const post = await Post.findByPk(id)
+
+            if (post.UserId !== req.session.userId) {
+                return res.render("403", { message: "You don't have access!" })
+            }
+
+            await Post.update(
+                { content },
+                { where: { id } }
+            )
+
+            await post.setTags([])
+
+            const hashtags = content.match(/#\w+/g)
+            if (hashtags) {
+                for (const tag of hashtags) {
+                    const [tagInstance] = await Tag.findOrCreate({
+                        where: { name: tag }
+                    })
+                    await post.addTag(tagInstance)
+                }
+            }
+
+            res.redirect(`/post/${id}`)
         } catch (error) {
             res.send(error)
         }
